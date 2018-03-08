41a40ff50a78bf804b331def20853c07e6beeb4c
DarienC <33129744+DarienC@users.noreply.github.com>
2018-03-08 11:22:28 -0500

    First Commit
    Created the project using the BH Adapter template. Built Properly

---
+++ SAP2000_Adapter/CRUD/Delete.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.Adapter.SAP2000
+{
+    public partial class SAP2000Adapter
+    {
+        /***************************************************/
+        /**** Adapter overload method                   ****/
+        /***************************************************/
+
+        protected override int Delete(Type type, IEnumerable<object> ids)
+        {
+            //Insert code here to enable deletion of specific types of objects with specific ids
+            return 0;
+        }
+
+        /***************************************************/
+    }
+}
---
+++ SAP2000_Adapter/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SAP2000_Adapter")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Buro Happold")]
+[assembly: AssemblyProduct("SAP2000_Adapter")]
+[assembly: AssemblyCopyright("Copyright © Buro Happold 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("04e7e3bc-bd2f-47a4-b4f0-a9675f702ced")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
