cda6c48b52bf5e40b57308d08b34d28c93c5e2f3
DarienC <33129744+DarienC@users.noreply.github.com>
2018-03-08 11:22:28 -0500

    First Commit
    Created the project using the BH Adapter template. Built Properly

---
+++ SAP2000_Engine/Convert/AdapterId.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.Engine.SAP2000
+{
+    public static partial class Convert
+    {
+        public const string AdapterId = "SAP2000_id";
+    }
+}
---
+++ SAP2000_Engine/Convert/ToSAP2000.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BH.oM.Structural.Elements;
+
+namespace BH.Engine.SAP2000
+{
+    public static partial class Convert
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        //Add methods for converting From BHoM to the specific software types, if possible to do without any BHoM calls
+        //Example:
+        //public static SAP2000Node ToSAP2000(this Node node)
+        //{
+        //    //Insert code for convertion
+        //}
+
+        /***************************************************/
+    }
+}
