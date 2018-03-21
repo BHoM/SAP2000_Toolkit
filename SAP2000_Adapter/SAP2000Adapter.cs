b634f6145b26a842a0da21bd7c154a53e82ddfdd
Josef Taylor <josef.taylor@burohappold.com>
2018-03-21 16:05:02 -0400

    Added SAP_Adapter Constructor

--- SAP2000_Adapter/SAP2000Adapter.cs
+++ SAP2000_Adapter/SAP2000Adapter.cs
@@ -5,26 +5,53 @@ using System.Text;
 using System.Threading.Tasks;
 using BH.Adapter;
 using BH.Engine.SAP2000;
+using SAP2000v19;
 
 namespace BH.Adapter.SAP2000
 {
     public partial class SAP2000Adapter : BHoMAdapter
     {
-
+        public const string ID = "SAP2000_id";
+        private cOAPI app;
+        private cSapModel model;
         /***************************************************/
         /**** Constructors                              ****/
         /***************************************************/
 
-        //Add any applicable constructors here, such as linking to a specific file or anything else as well as linking to that file through the (if existing) com link via the API
-        public SAP2000Adapter()
+        public SAP2000Adapter(string filePath = "")
         {
-            AdapterId = BH.Engine.SAP2000.Convert.AdapterId;   //Set the "AdapterId" to "SoftwareName_id". Generally stored as a constant string in the convert class in the SoftwareName_Engine
+            AdapterId = ID;
 
-            Config.SeparateProperties = true;   //Set to true to push dependant properties of objects before the main objects are being pushed. Example: push nodes before pushing bars
-            Config.MergeWithComparer = true;    //Set to true to use EqualityComparers to merge objects. Example: merge nodes in the same location
-            Config.ProcessInMemory = false;     //Set to false to to update objects in the toolkit during the push
-            Config.CloneBeforePush = true;      //Set to true to clone the objects before they are being pushed through the software. Required if any modifications at all, as adding a software ID is done to the objects
-            Config.UseAdapterId = true;         //Tag objects with a software specific id in the CustomData. Requires the NextIndex method to be overridden and implemented
+            Config.SeparateProperties = true;
+            Config.MergeWithComparer = true;
+            Config.ProcessInMemory = false;
+            Config.CloneBeforePush = true;
+
+            string pathToSAP = System.IO.Path.Combine(Environment.GetEnvironmentVariable("PROGRAMFILES"), "Computers and Structures", "SAP2000 19", "SAP2000.exe");
+            cHelper helper = new Helper();
+
+            object runningInstance = null;
+            runningInstance = System.Runtime.InteropServices.Marshal.GetActiveObject("CSI.SAP2000.API.SapObject");
+            if (runningInstance != null)
+            {
+                app = (cOAPI)runningInstance;
+                model = app.SapModel;
+                if (System.IO.File.Exists(filePath))
+                    model.File.OpenFile(filePath);
+                model.SetPresentUnits(eUnits.kN_m_C);
+            }
+            else
+            {
+                //open SAP if not running - NOTE: this behaviour is different from other adapters
+                app = helper.CreateObject(pathToSAP);
+                model = app.SapModel;
+                model.InitializeNewModel(eUnits.kN_m_C);
+                if (System.IO.File.Exists(filePath))
+                    model.File.OpenFile(filePath);
+                else
+                    model.File.NewBlank();
+            }
+
         }
 
 
