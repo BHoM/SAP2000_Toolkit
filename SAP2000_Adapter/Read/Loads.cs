25e6091bb1a933a38887431684baa0abeef0a60d
Josef Taylor <josef.taylor@burohappold.com>
2019-07-12 13:24:16 -0400

    Added Point, Bar, and Area loads

--- SAP2000_Adapter/Read/Loads.cs
+++ SAP2000_Adapter/Read/Loads.cs
@@ -1,8 +1,13 @@
-﻿using BH.oM.Structure.Loads;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System;
-using System.Collections.Generic;
-using SAP2000v19;
+using BH.oM.Structure.Elements;
+using BH.oM.Structure.Loads;
+using BH.oM.Geometry;
+using BH.oM.Base;
+using BH.Engine.Structure;
 using BH.Engine.SAP2000;
+using SAP2000v19;
 
 namespace BH.Adapter.SAP2000
 {
@@ -27,7 +32,7 @@ namespace BH.Adapter.SAP2000
 
                 if (m_model.LoadPatterns.GetLoadType(names[i], ref patternType) != 0)
                 {
-                    CreateElementError("Load Pattern", names[i]);
+                    ReadElementError("Load Pattern", names[i]);
                 }
                 else
                 {
@@ -42,47 +47,238 @@ namespace BH.Adapter.SAP2000
 
         private List<LoadCombination> ReadLoadCombination(List<string> ids = null)
         {
-            throw new NotImplementedException();
-            //List<LoadCombination> combinations = new List<LoadCombination>();
+            List<LoadCombination> combinations = new List<LoadCombination>();
 
-            //int number;
-            //int count = 0;
-            //string[] cName = null;
-            //double[] factors = null;
-            //int caseNum = 0;
-            //eCNameType[] nameTypes = null;
+            Dictionary<string, Loadcase> bhomCases = ReadLoadcase().ToDictionary(x => x.Name.ToString());
 
-            //foreach ( string name in ids)
-            //{
-            //    if (m_model.RespCombo.GetCaseList(name, ref count, ref nameTypes, ref cName, ref factors) == 0)
-            //    {
-            //        LoadCombination combination = new LoadCombination();
-            //        combination.LoadCases = cName.ToString();
-            //    }
-            //}
+            int count = 0;
+            string[] names = null;
 
+            m_model.RespCombo.GetNameList(ref count, ref names);
+            
+            for (int i = 0; i < count; i++)
+            {
+                double[] factors = null;
+                int caseCount = 0;
+                eCNameType[] caseTypes = null;
+                string[] caseNames = null;
 
-            //return combinations;
+                if (m_model.RespCombo.GetCaseList(names[i], ref caseCount, ref caseTypes, ref caseNames, ref factors) != 0)
+                {
+                    ReadElementError("Load Combo", names[i]);
+                }
+                else
+                {
+                    List<Loadcase> comboCases = new List<Loadcase>();
+                    foreach (string caseName in caseNames)
+                    {
+                        comboCases.Add(bhomCases[caseName]);
+                    }
+                    combinations.Add(BH.Engine.Structure.Create.LoadCombination(names[i], i, comboCases, factors.ToList()));
+                }
+            }
+
+            return combinations;
         }
 
         /***************************************************/
 
         private List<ILoad> ReadLoad(Type type, List<string> ids = null)
         {
-            // not implemented!
-            throw new NotImplementedException();
 
-            //List<ILoad> loadList = new List<ILoad>();
+            if (type == typeof(PointLoad))
+                return ReadPointLoad();
+            else if (type == typeof(BarUniformlyDistributedLoad))
+                return ReadBarLoad();
+            else if (type == typeof(AreaUniformlyDistributedLoad))
+                return ReadAreaLoad();
+            else
+            {
+                List<ILoad> loads = new List<ILoad>();
+                loads.AddRange(ReadPointLoad());
+                loads.AddRange(ReadBarLoad());
+                loads.AddRange(ReadAreaLoad());
+                return loads;
+            }         
+        }
 
-            ////get loadcases first
-            //List<Loadcase> loadcaseList = ReadLoadcase();
+        /***************************************************/
 
-            //loadList = Helper.GetLoads(model, loadcaseList);
+        private List<ILoad> ReadPointLoad(List<string> ids = null)
+        {
+            List<ILoad> loads = new List<ILoad>();
 
-            ////filter the list to return only the right type - No, this is not a clever way of doing it !
-            //loadList = loadList.Where(x => x.GetType() == type).ToList();
+            Dictionary<string, Loadcase> bhomCases = ReadLoadcase().ToDictionary(x => x.Name.ToString());
+            Dictionary<string, Node> bhomNodes = ReadNodes().ToDictionary(x => x.CustomData[AdapterId].ToString());
 
-            //return loadList;
+            int count = 0;
+            string[] nodeNames = null;
+            string[] caseNames = null;
+            int[] steps = null;
+            string[] cSys = null;
+            double[] f1 = null;
+            double[] f2 = null;
+            double[] f3 = null;
+            double[] m1 = null;
+            double[] m2 = null;
+            double[] m3 = null;
+
+
+            if (m_model.PointObj.GetLoadForce("All", ref count, ref nodeNames, ref caseNames, ref steps, ref cSys, ref f1, ref f2, ref f3, ref m1, ref m2, ref m3, eItemType.Group) == 0)
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    loads.Add(new PointLoad()
+                    {
+                        Force = new Vector() { X = f1[i], Y = f2[i], Z = f3[i] },
+                        Moment = new Vector() { X = m1[i], Y = m2[i], Z = m3[i] },
+                        Loadcase = bhomCases[caseNames[i]],
+                        Objects = new BHoMGroup<Node>() { Elements = { bhomNodes[nodeNames[i]] } }
+                    });
+                }
+            }
+
+
+
+            return loads;
+        }
+
+        /***************************************************/
+
+        private List<ILoad> ReadBarLoad(List<string> ids = null)
+        {
+            List<ILoad> loads = new List<ILoad>();
+
+            Dictionary<string, Loadcase> bhomCases = ReadLoadcase().ToDictionary(x => x.Name.ToString());
+            Dictionary<string, Bar> bhomBars = ReadBars().ToDictionary(x => x.CustomData[AdapterId].ToString());
+
+            int count = 0;
+            string[] frameNames = null;
+            string[] caseNames = null;
+            int[] myTypes = null;
+            string[] cSys = null;
+            int[] dir = null;
+            double[] rd1 = null;
+            double[] rd2 = null;
+            double[] dist1 = null;
+            double[] dist2 = null;
+            double[] val1 = null;
+            double[] val2 = null;
+
+
+            if (m_model.FrameObj.GetLoadDistributed("All", ref count, ref frameNames, ref caseNames, ref myTypes, ref cSys, ref dir, ref rd1, ref rd2, ref dist1, ref dist2, ref val1, ref val2, eItemType.Group) == 0)
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    Bar bhomBar = bhomBars[frameNames[i]];
+
+                    if (dist1[i] != 0 || rd1[i] != 0 || dist2[i] != bhomBar.Length() || rd2[i] != 1)
+                    {
+                        Engine.Reflection.Compute.RecordWarning("Partial distributed loads are not supported. Smearing load all over the bar like jelly.");
+                    }
+                    double val = ((val1[i] + val2[i]) / 2) * (dist2[i] - dist1[i]) / bhomBar.Length();
+                    Vector force = new Vector();
+                    switch (dir[i])
+                    {
+                        case 4:
+                            force.X = val;
+                            break;
+                        case 5:
+                            force.Y = val;
+                            break;
+                        case 6:
+                            force.Z = val;
+                            break;
+                        case 10:
+                            force.Z = -val;
+                            break;
+                        default:
+                            BH.Engine.Reflection.Compute.RecordWarning("That load direction is not supported. Dir = " + dir[i].ToString());
+                            break;
+                    }
+                    switch (myTypes[i])
+                    {
+                        case 1:
+                            loads.Add(new BarUniformlyDistributedLoad()
+                            {
+                                Force = force,
+                                Loadcase = bhomCases[caseNames[i]],
+                                Objects = new BHoMGroup<Bar>() { Elements = { bhomBar } }
+                            });
+                            break;
+                        case 2:
+                            loads.Add(new BarUniformlyDistributedLoad()
+                            {
+                                Moment = force,
+                                Loadcase = bhomCases[caseNames[i]],
+                                Objects = new BHoMGroup<Bar>() { Elements = { bhomBar } }
+                            });
+                            break;
+                        default:
+                            BH.Engine.Reflection.Compute.RecordWarning("Could not create the load. It's not 'MyType'. MyType = " + myTypes[i].ToString());
+                            break;
+                    }
+                }
+            }
+
+
+
+            return loads;
+        }
+
+        /***************************************************/
+
+        private List<ILoad> ReadAreaLoad(List<string> ids = null)
+        {
+            List<ILoad> loads = new List<ILoad>();
+
+            Dictionary<string, Loadcase> bhomCases = ReadLoadcase().ToDictionary(x => x.Name.ToString());
+            Dictionary<string, Panel> bhomPanels = ReadPanel().ToDictionary(x => x.CustomData[AdapterId].ToString());
+
+            int count = 0;
+            string[] areaNames = null;
+            string[] caseNames = null;
+            string[] cSys = null;
+            int[] dir = null;
+            double[] val = null;
+
+
+            if (m_model.AreaObj.GetLoadUniform("All", ref count, ref areaNames, ref caseNames, ref cSys, ref dir, ref val, eItemType.Group) == 0)
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    Panel bhomPanel = bhomPanels[areaNames[i]];
+                    
+                    Vector force = new Vector();
+                    switch (dir[i])
+                    {
+                        case 4:
+                            force.X = val[i];
+                            break;
+                        case 5:
+                            force.Y = val[i];
+                            break;
+                        case 6:
+                            force.Z = val[i];
+                            break;
+                        case 10:
+                            force.Z = -val[i];
+                            break;
+                        default:
+                            BH.Engine.Reflection.Compute.RecordWarning("That load direction is not supported. Dir = " + dir[i].ToString());
+                            break;
+                    }
+
+                    loads.Add(new AreaUniformlyDistributedLoad()
+                    {
+                        Pressure = force,
+                        Loadcase = bhomCases[caseNames[i]],
+                        Objects = new BHoMGroup<IAreaElement>() { Elements = { bhomPanel as IAreaElement} }
+                    });
+                }
+            }
+
+            return loads;
         }
 
         /***************************************************/
