77303fee3af90bf1c54d673f7822ef4908b21df8
Josef Taylor <josef.taylor@burohappold.com>
2019-07-18 08:19:58 -0400

    Added Create methods
    assumed all coordinate systems are global at this point. Need to implement coordinate system ToCSI/toBHoM

--- SAP2000_Adapter/Create/Loads.cs
+++ SAP2000_Adapter/Create/Loads.cs
@@ -1,4 +1,10 @@
-﻿using BH.oM.Structure.Loads;
+﻿using BH.Engine.SAP2000;
+using BH.oM.Structure.Elements;
+using BH.oM.Structure.Loads;
+using SAP2000v19;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace BH.Adapter.SAP2000
 {
@@ -10,44 +16,140 @@ namespace BH.Adapter.SAP2000
 
         private bool CreateObject(Loadcase loadcase)
         {
-            // not implemented!
-            CreateElementError("Loads Not Implemented!", loadcase.Name);
-            return false;
-            //bool success = true;
-
-            //Helper.SetLoadcase(model, loadcase);
-
-            //return success;
+            double selfWeight = 0;
+            if (loadcase.Nature == LoadNature.Dead)
+                selfWeight = 1;
+            if (m_model.LoadPatterns.Add(loadcase.Name, loadcase.Nature.ToCSI(), selfWeight, true) != 0)
+                CreateElementError("LoadCase", loadcase.Name);
+            return true;
         }
 
         /***************************************************/
 
         private bool CreateObject(LoadCombination loadcombination)
         {
-            // not implemented!
-            CreateElementError("Loads Not Implemented!", loadcombination.Name);
-            return false;
-            //bool success = true;
+            string name = loadcombination.Name;
+            eCNameType nameType = eCNameType.LoadCase;
+            if (m_model.RespCombo.Add(name, 0) == 0)
+            {
+                List<Tuple<double, ICase>> bhomCases = loadcombination.LoadCases;
+                foreach (Tuple<double, ICase> comboCase in loadcombination.LoadCases)
+                {
+                    double factor = comboCase.Item1;
+                    ICase bhomCase = comboCase.Item2;
+                    if (m_model.RespCombo.SetCaseList(name, ref nameType, bhomCase.Name, factor) != 0)
+                        Engine.Reflection.Compute.RecordWarning("Could not add case " + bhomCase.Name + " to combo " + name);
+                }
+            }
+            else
+            {
+                CreateElementError("Load Combination", name);
+            }
 
-            //Helper.SetLoadCombination(model, loadcombination);
-
-            //return success;
+            return true;
         }
 
         /***************************************************/
 
         private bool CreateObject(ILoad bhLoad)
         {
-            // not implemented!
-            CreateElementError("Loads Not Implemented!", bhLoad.Name);
-            return false;
-            //bool success = true;
+            return CreateLoad(bhLoad as dynamic);
+        }
 
-            //Helper.SetLoad(model, bhLoad as dynamic);
+        /***************************************************/
 
+        private bool CreateLoad(PointLoad bhLoad)
+        {
+            List<Node> nodes = bhLoad.Objects.Elements.ToList();
+            string loadPat = bhLoad.Loadcase.CustomData[AdapterId].ToString();
+            if (bhLoad.Axis != LoadAxis.Global)
+                Engine.Reflection.Compute.RecordWarning("Local axis loads are not yet supported. Applying in Global Axis");
+            double[] val = 
+            {
+                bhLoad.Force.X,
+                bhLoad.Force.Y,
+                bhLoad.Force.Z,
+                bhLoad.Moment.X,
+                bhLoad.Moment.Y,
+                bhLoad.Moment.Z,
+            };
 
-            //return success;
+            bool replace = true;
+            string cSys = "Global";
 
+            foreach (Node bhNode in nodes)
+            {
+                string name = bhNode.CustomData[AdapterId].ToString();
+                if (m_model.PointObj.SetLoadForce(name, loadPat, ref val, replace, cSys, eItemType.Objects) != 0)
+                    CreateElementError("Point Load", name);
+            }
+
+            return true;
+        }
+
+        /***************************************************/
+
+        private bool CreateLoad(BarUniformlyDistributedLoad bhLoad)
+        {
+            List<Bar> bars = bhLoad.Objects.Elements.ToList();
+            string loadPat = bhLoad.Loadcase.CustomData[AdapterId].ToString();
+            double dist1 = 0;
+            double dist2 = 1;
+            int[] dirs = { 4, 5, 6 };
+            bool relDist = true;
+            string cSys = "Global";
+            if (bhLoad.Axis != LoadAxis.Global)
+                Engine.Reflection.Compute.RecordWarning("Local axis loads are not yet supported. Applying in Global Axis");
+            eItemType type = eItemType.Objects;
+            bool replace = true;
+            
+            double[] forceVals = bhLoad.Force.ToDoubleArray();
+            double[] momentVals = bhLoad.Moment.ToDoubleArray();
+
+            foreach (Bar bhBar in bars)
+            {
+                string name = bhBar.CustomData[AdapterId].ToString();
+                bool replaceNow = replace;
+                for (int i = 0; i < dirs.Count(); i++)
+                {
+                    if (m_model.FrameObj.SetLoadDistributed(name, loadPat, 1, dirs[i], dist1, dist2, forceVals[i], forceVals[i], cSys, relDist, replaceNow, type) != 0)
+                        CreateElementError("BarLoad", bhBar.Name + dirs[i]);
+                    replaceNow = false;
+                    if (m_model.FrameObj.SetLoadDistributed(name, loadPat, 2, dirs[i], dist1, dist2, momentVals[i], momentVals[i], cSys, relDist, replaceNow, type) != 0)
+                        CreateElementError("BarLoad", bhBar.Name + dirs[i]);
+                }
+            }
+
+            return true;
+        }
+
+        /***************************************************/
+
+        private bool CreateLoad(AreaUniformlyDistributedLoad bhLoad)
+        {
+            List<IAreaElement> panels = bhLoad.Objects.Elements.ToList();
+            string loadPat = bhLoad.Loadcase.CustomData[AdapterId].ToString();
+            double[] vals = bhLoad.Pressure.ToDoubleArray();
+            int[] dirs = { 4, 5, 6 };
+            bool replace = true;
+            string cSys = "Global";
+            if (bhLoad.Axis != LoadAxis.Global)
+                Engine.Reflection.Compute.RecordWarning("Local axis loads are not yet supported. Applying in Global Axis");
+            eItemType type = eItemType.Objects;
+
+            foreach (Panel panel in panels)
+            {
+                string name = panel.CustomData[AdapterId].ToString();
+                bool replaceNow = replace;
+                for (int i = 0; i < dirs.Count(); i++)
+                {
+                    if (m_model.AreaObj.SetLoadUniform(name, loadPat, vals[i], dirs[i], replaceNow, cSys, type) != 0)
+                        CreateElementError("AreaLoad Dir", panel.Name + dirs[i]);
+                    replaceNow = false;
+                }
+            }
+
+            return true;
         }
 
         /***************************************************/
---
+++ SAP2000_Engine/Convert/ToSAP2000/Loads.cs
@@ -0,0 +1,58 @@
+﻿using BH.oM.Structure.Loads;
+using SAP2000v19;
+
+namespace BH.Engine.SAP2000
+{
+    public static partial class Convert
+    {   
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static eLoadPatternType ToCSI(this LoadNature loadNature)
+        {
+            eLoadPatternType patType = new eLoadPatternType();
+
+            switch (loadNature)
+            {
+                case LoadNature.Dead:
+                    patType = eLoadPatternType.Dead;
+                    break;
+                case LoadNature.SuperDead:
+                    patType = eLoadPatternType.SuperDead;
+                    break;
+                case LoadNature.Live:
+                    patType = eLoadPatternType.Live;
+                    break;
+                case LoadNature.Seismic:
+                    patType = eLoadPatternType.Quake;
+                    break;
+                case LoadNature.Wind:
+                    patType = eLoadPatternType.Wind;
+                    break;
+                case LoadNature.Snow:
+                    patType = eLoadPatternType.Snow;
+                    break;
+                case LoadNature.Other:
+                    patType = eLoadPatternType.Other;
+                    break;
+                case LoadNature.Temperature:
+                    patType = eLoadPatternType.Temperature;
+                    break;
+                case LoadNature.Notional:
+                    patType = eLoadPatternType.Notional;
+                    break;
+                case LoadNature.Prestress:
+                    patType = eLoadPatternType.Prestress;
+                    break;
+                default:
+                    patType = eLoadPatternType.Other;
+                    break;
+            }
+
+            return patType;
+        }
+
+        /***************************************************/
+    }
+}
