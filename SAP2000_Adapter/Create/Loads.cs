316302a064bc2795485450e5a0c7c1d4a8d4a848
Josef Taylor <josef.taylor@burohappold.com>
2019-08-22 12:16:59 -0400

    set id for loadcases

--- SAP2000_Adapter/Create/Loads.cs
+++ SAP2000_Adapter/Create/Loads.cs
@@ -21,6 +21,7 @@ namespace BH.Adapter.SAP2000
                 selfWeight = 1;
             if (m_model.LoadPatterns.Add(loadcase.Name, loadcase.Nature.ToCSI(), selfWeight, true) != 0)
                 CreateElementError("LoadCase", loadcase.Name);
+            loadcase.CustomData[AdapterId] = loadcase.Name;
             return true;
         }
 
