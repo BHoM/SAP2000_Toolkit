ac41a25d97e071766be6f09356cb8eeaadcdc7cb
Maryanne Wachter <maryanne.wachter@burohappold.com>
2020-01-28 22:07:20 -0800

    bar results first commit

---
+++ SAP2000_Adapter/CRUD/Read/Results/BarResults.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BH.oM.Structure.Results;
+using BH.oM.Common;
+using BH.oM.Structure.Elements;
+using BH.oM.Adapters.SAP2000;
+using BH.Engine.SAP2000;
+using BH.oM.Structure.Loads;
+using BH.oM.Structure.Requests;
+using BH.oM.Geometry;
+using BH.Engine.Geometry;
+
+namespace BH.Adapter.SAP2000
+{
+    public IEnumerable<IResult> ReadResults(BarResultRequest request)
+    {
+
+    }
+}
---
+++ SAP2000_Adapter/CRUD/Read/Results/Result.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BH.oM.Structure.Results;
+using BH.oM.Common;
+using BH.oM.Structure.Elements;
+using BH.oM.Adapters.SAP2000;
+using BH.oM.Structure.Loads;
+using BH.oM.Data.Requests;
+
+namespace BH.Adapter.SAP2000
+{
+    public partial class SAP2000Adapter
+    {
+        protected override IEnumerable<IResult> ReadResults(Type type, IList ids = null, IList cases = null, int divisions = 5)
+        {
+            IResultRequest request = Engine.Structure.Create.IResultRequest(type, ids?.Cast<object>(), cases?.Cast<object>(), divisions);
+
+            if (request != null)
+                return this.ReadResults(request as dynamic);
+            else
+                return new List<IResult>();
+        }
+    }
