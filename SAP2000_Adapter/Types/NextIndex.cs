bf70c486f4883dd9480c3d5d2cda1f1e4472fe90
Josef Taylor <josef.taylor@burohappold.com>
2020-10-28 22:58:03 -0400

    Added NextIndex override

--- SAP2000_Adapter/Types/NextIndex.cs
+++ SAP2000_Adapter/Types/NextIndex.cs
@@ -20,42 +20,23 @@
  * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
  */
 
-using BH.Engine.Base.Objects;
-using BH.oM.Structure.MaterialFragments;
-using BH.oM.Structure.Elements;
-using BH.oM.Structure.Constraints;
-using BH.oM.Structure.SectionProperties;
-using BH.oM.Structure.SurfaceProperties;
-using BH.oM.Structure.Loads;
 using System;
+using BH.Engine.Adapter;
+using BH.oM.Adapters.SAP2000;
 using System.Collections.Generic;
-using BH.Engine.Structure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace BH.Adapter.SAP2000
 {
     public partial class SAP2000Adapter : BHoMAdapter
     {
-        /***************************************************/
-        /**** BHoM Adapter Interface                    ****/
-        /***************************************************/
-        
-        //Compares nodes by distance (down to 3 decimal places -> mm)
-        //Compares Materials, SectionProperties, LinkConstraints, and Property2D by name
-        //Add/remove any type in the dictionary below that you want (or not) a specific comparison method for.
-
-        private void SetupComparers()
+        protected override object NextFreeId(Type objectType, bool refresh)
         {
-            AdapterComparers = new Dictionary<Type, object>
-            {
-                {typeof(Node), new NodeDistanceComparer(3) },   //The 3 in here sets how many decimal places to look at for node merging. 3 decimal places gives mm precision
-                {typeof(ISectionProperty), new NameOrDescriptionComparer() },
-                {typeof(IMaterialFragment), new NameOrDescriptionComparer() },
-                {typeof(LinkConstraint), new NameOrDescriptionComparer() },
-                {typeof(ISurfaceProperty), new NameOrDescriptionComparer() },
-                {typeof(ICase), new BHoMObjectNameComparer() }, 
-            };
+            return -1;
         }
 
-        /***************************************************/
     }
 }
+
