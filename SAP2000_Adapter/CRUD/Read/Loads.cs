2c1dae6e29a582c347e1afd7b610ecf8da10a8f2
Isak Naslund <Isak.Naslund@burohappold.com>
2020-11-16 18:20:51 +0100

    Update BarVaryingLoad

--- SAP2000_Adapter/CRUD/Read/Loads.cs
+++ SAP2000_Adapter/CRUD/Read/Loads.cs
@@ -431,10 +431,10 @@ namespace BH.Adapter.SAP2000
                             case 1:
                                 loads.Add(new BarVaryingDistributedLoad()
                                 {
-                                    DistanceFromA = dist1[i],
-                                    ForceA = forceA,
-                                    DistanceFromB = dist2[i],
-                                    ForceB = forceB,
+                                    StartPosition = dist1[i],
+                                    ForceAtStart = forceA,
+                                    EndPosition = dist2[i],
+                                    ForceAtEnd = forceB,
                                     Loadcase = bhomCases[caseNames[i]],
                                     Objects = new BHoMGroup<Bar>() { Elements = { bhomBar } },
                                     Axis = axis
@@ -443,10 +443,10 @@ namespace BH.Adapter.SAP2000
                             case 2:
                                 loads.Add(new BarVaryingDistributedLoad()
                                 {
-                                    DistanceFromA = dist1[i],
-                                    MomentA = forceA,
-                                    DistanceFromB = dist2[i],
-                                    MomentB = forceB,
+                                    StartPosition = dist1[i],
+                                    MomentAtStart = forceA,
+                                    EndPosition = dist2[i],
+                                    MomentAtEnd = forceB,
                                     Loadcase = bhomCases[caseNames[i]],
                                     Objects = new BHoMGroup<Bar>() { Elements = { bhomBar } },
                                     Axis = axis
