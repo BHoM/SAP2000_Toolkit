4c4c642ad9159a2da0b8c6ad40a35c44a5604784
Josef Taylor <josef.taylor@burohappold.com>
2021-12-29 15:19:04 -0500

    Getting fragment properties on one line each

--- SAP2000_oM/Fragments/PanelOffsetByJointPattern.cs
+++ SAP2000_oM/Fragments/PanelOffsetByJointPattern.cs
@@ -27,21 +27,9 @@ namespace BH.oM.Adapters.SAP2000.Fragments
     public class PanelOffsetByJointPattern : IPanelOffset
     {
         [Description("This is the name of the defined joint pattern that is used to calculate the joint offsets.")]
-        public virtual string OffsetPattern
-        {
-            get;
-            set;
-        }
-
-        = "";
+        public virtual string OffsetPattern { get; set; } = "";
         [Length]
         [Description("This is the scale factor applied to the joint pattern when calculating the joint offsets.")]
-        public virtual double OffsetPatternSF
-        {
-            get;
-            set;
-        }
-
-        = 0;
+        public virtual double OffsetPatternSF { get; set; } = 0;
     }
 }
